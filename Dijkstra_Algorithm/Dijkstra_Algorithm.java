c3c79bb4acdac9b5a2cf908cea999cbf9161f30d
Amitsharma45 <sharma.amit20111@gmail.com>
2020-10-01 14:04:23 +0530

    complete

--- Dijkstra_Algorithm/Dijkstra_Algorithm.java
+++ Dijkstra_Algorithm/Dijkstra_Algorithm.java
@@ -1,97 +1,134 @@
-package Greedy.Dijkstra;
+// Java implementation of Dijkstra's Algorithm 
+// using Priority Queue 
+import java.util.*; 
+public class DPQ { 
+	private int dist[]; 
+	private Set<Integer> settled; 
+	private PriorityQueue<Node> pq; 
+	private int V; // Number of vertices 
+	List<List<Node> > adj; 
 
-import java.lang.*;
-// A Java program for Dijkstra's single source shortest path algorithm.
-// The program uses adjacency matrix
-    class Dijkstra
-    {
+	public DPQ(int V) 
+	{ 
+		this.V = V; 
+		dist = new int[V]; 
+		settled = new HashSet<Integer>(); 
+		pq = new PriorityQueue<Node>(V, new Node()); 
+	} 
 
-        static final int V=9; //number of vertices
-        // A function to find the vertex with minimum distance value,
-        // from the set of vertices that aren't yet included in shortest path tree
-        void dj_algo(int graph[][], int source)
-        {
-            int distance[] = new int[V];
-            
-           
-            Boolean sptIncluded[] = new Boolean[V];
-            // Initializing all distances as INFINITE and stpIncluded[] as false
-            for (int i = 0; i < V; i++)
-            {
-                distance[i] = Integer.MAX_VALUE;
-                sptIncluded[i] = false;
-            }
-            distance[source] = 0;
-            // Shortest path
-            for (int count = 0; count < V-1; count++)
-            {
-                // Choose the vertex with smallest distance from unprocessed vertices
-                int u = minDistance(distance, sptIncluded);
-                // Mark the picked vertex as processed
-                sptIncluded[u] = true;
-                // Update distance value of the adjacent vertices of the picked vertex.
-                for (int v = 0; v < V; v++)
-                    // Update distance[v] only if it is not in sptIncluded,
-                    // there is an edge from u to v, and total distance(weight) from source to
-                    // v through u is smaller than current value of distance[v]
-                    // code courtesy Geeksforgeeks.org
-                    if (!sptIncluded[v] && graph[u][v]!=0 &&
-                            distance[u] != Integer.MAX_VALUE &&
-                            distance[u]+graph[u][v] < distance[v])
-                        distance[v] = distance[u] + graph[u][v];
-            }
-            // Generate the output
-            output(distance);
-        }
-        int minDistance(int distance[], Boolean sptIncluded[])
-        {
-            // Initialize minimum value
-            //Take the reference from Negative Infinity
-            //min_index corresponds to the index of
-            //vertex whose distance is stored in min
-            int min = Integer.MAX_VALUE, min_index=-1;
-            for (int v = 0; v < V; v++) {
-                if (sptIncluded[v] == false && distance[v] <= min) {
-                    min = distance[v];
-                    min_index = v;
-                }
-            }
-            return min_index;
-        }
-        void output(int distance[]){
-            System.out.println("Vertex\tDistance From Source");
-            for (int i = 0; i < V; i++) {
-                System.out.println(i + "\t\t\t" + distance[i]);
-            }
-        }
-        // Driver method
-        public static void main (String[] args)
-        {
-        /* Example graph (adjacency matrix)*/
-        // courtesy Geeksforgeeks.org
-            int adj_matrix[][] = new int[][]{
-                    {0, 4, 0, 6, 0, 3, 0, 8, 0},
-                    {4, 0, 8, 0, 0, 0, 0, 7, 0},
-                    {0, 8, 0, 4, 0, 4, 0, 0, 2},
-                    {6, 0, 4, 0, 9, 5, 0, 0, 0},
-                    {0, 0, 0, 9, 0, 9, 0, 0, 0},
-                    {3, 0, 4, 5, 9, 0, 2, 0, 0},
-                    {0, 0, 0, 0, 0, 2, 0, 1, 6},
-                    {8, 7, 0, 0, 0, 0, 1, 0, 7},
-                    {0, 0, 2, 0, 0, 0, 6, 7, 0}
-            };
-            System.out.println("Output");
-            Dijkstra d = new Dijkstra();
-            d.dj_algo(adj_matrix, 0);
-        }
-    }
-// Output
-//        0		0
-//        1		4
-//        2		7
-//        3		6
-//        4		12
-//        5		3
-//        6		5
-//        7		6
-//        8		9
+	// Function for Dijkstra's Algorithm 
+	public void dijkstra(List<List<Node> > adj, int src) 
+	{ 
+		this.adj = adj; 
+
+		for (int i = 0; i < V; i++) 
+			dist[i] = Integer.MAX_VALUE; 
+
+		// Add source node to the priority queue 
+		pq.add(new Node(src, 0)); 
+
+		// Distance to the source is 0 
+		dist[src] = 0; 
+		while (settled.size() != V) { 
+
+			// remove the minimum distance node 
+			// from the priority queue 
+			int u = pq.remove().node; 
+
+			// adding the node whose distance is 
+			// finalized 
+			settled.add(u); 
+
+			e_Neighbours(u); 
+		} 
+	} 
+
+	// Function to process all the neighbours 
+	// of the passed node 
+	private void e_Neighbours(int u) 
+	{ 
+		int edgeDistance = -1; 
+		int newDistance = -1; 
+
+		// All the neighbors of v 
+		for (int i = 0; i < adj.get(u).size(); i++) { 
+			Node v = adj.get(u).get(i); 
+
+			// If current node hasn't already been processed 
+			if (!settled.contains(v.node)) { 
+				edgeDistance = v.cost; 
+				newDistance = dist[u] + edgeDistance; 
+
+				// If new distance is cheaper in cost 
+				if (newDistance < dist[v.node]) 
+					dist[v.node] = newDistance; 
+
+				// Add the current node to the queue 
+				pq.add(new Node(v.node, dist[v.node])); 
+			} 
+		} 
+	} 
+
+	// Driver code 
+	public static void main(String arg[]) 
+	{ 
+		int V = 5; 
+		int source = 0; 
+
+		// Adjacency list representation of the 
+		// connected edges 
+		List<List<Node> > adj = new ArrayList<List<Node> >(); 
+
+		// Initialize list for every node 
+		for (int i = 0; i < V; i++) { 
+			List<Node> item = new ArrayList<Node>(); 
+			adj.add(item); 
+		} 
+
+		// Inputs for the DPQ graph 
+		adj.get(0).add(new Node(1, 9)); 
+		adj.get(0).add(new Node(2, 6)); 
+		adj.get(0).add(new Node(3, 5)); 
+		adj.get(0).add(new Node(4, 3)); 
+
+		adj.get(2).add(new Node(1, 2)); 
+		adj.get(2).add(new Node(3, 4)); 
+
+		// Calculate the single source shortest path 
+		DPQ dpq = new DPQ(V); 
+		dpq.dijkstra(adj, source); 
+
+		// Print the shortest path to all the nodes 
+		// from the source node 
+		System.out.println("The shorted path from node :"); 
+		for (int i = 0; i < dpq.dist.length; i++) 
+			System.out.println(source + " to " + i + " is "
+							+ dpq.dist[i]); 
+	} 
+} 
+
+// Class to represent a node in the graph 
+class Node implements Comparator<Node> { 
+	public int node; 
+	public int cost; 
+
+	public Node() 
+	{ 
+	} 
+
+	public Node(int node, int cost) 
+	{ 
+		this.node = node; 
+		this.cost = cost; 
+	} 
+
+	@Override
+	public int compare(Node node1, Node node2) 
+	{ 
+		if (node1.cost < node2.cost) 
+			return -1; 
+		if (node1.cost > node2.cost) 
+			return 1; 
+		return 0; 
+	} 
+} 
